abc6aeac9add1e046b362f74339883de35e729b5
Arne Molland <arneolaim@gmail.com>
2019-08-29 00:39:35 +0200

    📦 Added react-lincoln
    Also changed footer text

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,7 @@ import './footer.css'
 export default function Footer(props) {
     return (
         <div className="footer">
-            <p>&copy; Arne Molland 2019. All rights reserved.</p>
+            <p>&copy; horarium 2019. All rights reserved.</p>
         </div>
     )
 }
